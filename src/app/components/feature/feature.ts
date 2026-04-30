1c0f9302b9f773f2a79a933bcb96b0fe754298c7
nicola.camuffo <nicola.camuffo@gmail.com>
2026-04-30 19:28:46 +0200

    creazione componente Feature e stilizzazione

---
+++ src/app/components/feature/feature.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature',
+  imports: [],
+  templateUrl: './feature.html',
+  styleUrl: './feature.scss',
+})
+export class Feature {}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Banner } from '../../components/banner/banner';
+import { Feature } from '../../components/feature/feature';
 
 @Component({
   selector: 'app-home',
-  imports: [Banner],
+  imports: [Banner, Feature],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrl: './home.scss'
 })
-export class Home {}
+export class Home {
+}
