5fe0dbd4fc73e6ca824820f1536c04d814602ccf
nicola.camuffo <nicola.camuffo@gmail.com>
2026-05-01 18:22:38 +0200

    refactoring sidebar

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,8 +1,9 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs/operators';
+import { BREAKPOINTS } from '../../models/breakpoints';
 
 @Component({
   selector: 'app-sidebar',
@@ -11,43 +12,40 @@ import { map } from 'rxjs/operators';
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
-  private readonly MY_BREAKPOINTS = {
-    xlarge: '(min-width: 1281px) and (max-width: 1680px)',
-    large: '(min-width: 981px) and (max-width: 1280px)',
-    medium: '(min-width: 737px) and (max-width: 980px)',
-    small: '(min-width: 481px) and (max-width: 736px)',
-    xsmall: '(min-width: 361px) and (max-width: 480px)',
-    xxsmall: '(max-width: 360px)'
-  };
-
-
   private breakpointObserver = inject(BreakpointObserver);
 
-  sidebarOpen = signal(false);
 
-  isLarge = toSignal(
+  private isOpen = signal(true);
+
+  private isAtMostLarge = toSignal(
     this.breakpointObserver.observe([
-      this.MY_BREAKPOINTS.large,
-      this.MY_BREAKPOINTS.medium,
-      this.MY_BREAKPOINTS.small,
-      this.MY_BREAKPOINTS.xsmall,
-      this.MY_BREAKPOINTS.xxsmall
+      BREAKPOINTS.large,
+      BREAKPOINTS.medium,
+      BREAKPOINTS.small,
+      BREAKPOINTS.xsmall,
+      BREAKPOINTS.xxsmall
     ]).pipe(
       map(result => result.matches)
     ),
     { initialValue: false }
   );
 
-  isSidebarInactive = computed(() => {
-    if (this.isLarge())
-      return false;
 
-    return !this.sidebarOpen();
+  isInactive = computed(() => {
+    return !this.isOpen();
   });
 
 
-  toggleSidebar(event: Event) {
+  constructor() {
+    effect(() => {
+      if (this.isAtMostLarge())
+        this.isOpen.set(false);
+    });
+  }
+
+
+  toggle(event: Event) {
     event.preventDefault();
-    this.sidebarOpen.update(v => !v);
+    this.isOpen.update(v => !v);
   }
 }
---
+++ src/app/models/breakpoints.ts
@@ -0,0 +1,8 @@
+export const BREAKPOINTS = {
+  xlarge: '(min-width: 1281px) and (max-width: 1680px)',
+  large: '(min-width: 981px) and (max-width: 1280px)',
+  medium: '(min-width: 737px) and (max-width: 980px)',
+  small: '(min-width: 481px) and (max-width: 736px)',
+  xsmall: '(min-width: 361px) and (max-width: 480px)',
+  xxsmall: '(max-width: 360px)'
+};
