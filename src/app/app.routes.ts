0d614ffd2505ebb08391cdf0bb6c7f368d88977b
nicola.camuffo <nicola.camuffo@gmail.com>
2026-05-01 15:23:49 +0200

    creazione componente Article e stilizzazione; integrazione routing; fix stile bordo superiore (primo 'section')

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
+import { Article } from './pages/article/article';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
+  { path: 'article', component: Article },
   { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
 ];
--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.scss',
+  styleUrl: './sidebar.scss'
 })
-export class Sidebar {}
+export class Sidebar {
+}
