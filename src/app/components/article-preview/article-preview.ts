229aea731f05ce2884b24bfbe808a4023187f71d
nicola.camuffo <nicola.camuffo@gmail.com>
2026-05-01 14:43:32 +0200

    creazione componente ArticlePreview e stilizzazione

---
+++ src/app/components/article-preview/article-preview.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-preview',
+  imports: [],
+  templateUrl: './article-preview.html',
+  styleUrl: './article-preview.scss',
+})
+export class ArticlePreview {}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Banner } from '../../components/banner/banner';
 import { Feature } from '../../components/feature/feature';
+import { ArticlePreview } from '../../components/article-preview/article-preview';
 
 @Component({
   selector: 'app-home',
-  imports: [Banner, Feature],
+  imports: [Banner, Feature, ArticlePreview],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
