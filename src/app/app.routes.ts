79e496d12952e8da12e6661e38d64b15ef95509f
nicola.camuffo <nicola.camuffo@gmail.com>
2026-06-01 19:01:27 +0200

    pagine About e Contact + semplificazione voci sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
 import { Article } from './pages/article/article';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'article', component: Article },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
   { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
 ];
