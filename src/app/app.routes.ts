a241a2ce4b33053621465af376f408139c5f40db
pizzipe <nicola.camuffo82@gmail.com>
2026-08-13 13:12:12 +0200

    pagina Categories

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
+import { Categories } from './pages/categories/categories';
 import { Article } from './pages/article/article';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
@@ -8,6 +9,7 @@ import { Contact } from './pages/contact/contact';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
+  { path: 'categories', component: Categories },
   { path: 'article', component: Article },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
